fbe06687eff17756d7738ad74ad845e8a0570f01
badxbadger <>
2026-02-15 16:43:50 +0000

    Challenge: Inventory Sorting Capstone

---
+++ variables_and_types/challenge_inventory_sorting_capstone/main.py
@@ -0,0 +1,17 @@
+# Lists of items and categories for slicing
+items = "Bubblegum, Chocolate, Pasta"
+categories = "Candy Aisle, Pasta Aisle"
+candy1 = items[0:11]
+candy2 = items[12:22]
+candy3 = items[13:]
+category1 = categories[0:11]
+category2 = categories[12:]
+bubblegum_price = "1.50"
+chocolate_price = "2.00"
+pasta_price = "5.40"
+print(items)
+print(categories)
+print(bubblegum_price)
+print(chocolate_price)
+print(pasta_price)
+print(f("We have candy1 for bubblegum_price in the category1 "))
