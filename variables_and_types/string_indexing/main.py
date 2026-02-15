f6ba7b92d3a14b599ce93646f4a9d87b86593c3f
badxbadger <>
2026-02-15 14:23:23 +0000

    String Indexing

---
+++ variables_and_types/string_indexing/main.py
@@ -0,0 +1,6 @@
+grocery_item = "Grilled Chicken Salad"
+length_of_item = len(grocery_item)
+first_character = grocery_item[0]
+second_character = grocery_item[1]
+third_character = grocery_item[2]
+# Testing
